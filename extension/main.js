7f7265ee7de231f54eb3a53cb3fc0da332bcfc0e
Paul Chihak <perihwk@users.noreply.github.com>
2024-10-11 16:12:19 -0500

    postMessage example

--- extension/main.js
+++ extension/main.js
@@ -1,9 +1,24 @@
+let nativeHost = null
+
 function sendNativeMessage() {
     chrome.runtime.sendNativeMessage('com.example.messaging', {
         query: 'hello-world',
     }).then((e) => { console.log(e) })
 }
 
+function establishNativeConnection(){
+    nativeHost = chrome.runtime.connectNative('com.example.messaging')
+}
+
+function destroyNativeConnection(){
+    if (nativeHost) {
+        nativeHost.disconnect();
+        nativeHost = null;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('send-message-button').addEventListener('click', sendNativeMessage);
+    document.getElementById('create-connection').addEventListener('click', establishNativeConnection);
+	document.getElementById('disconnect').addEventListener('click', destroyNativeConnection);
+	document.getElementById('send-single-message').addEventListener('click', sendNativeMessage);
 })
